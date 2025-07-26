74324dbb874a4d300920e32b785d38e9cfcc9092
Tech Turtle <elyseeumukunzi@gmail.com>
2025-07-26 13:51:41 +0200

    ...

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
+define('DB_HOST','localhost'); //all these will change on server side
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','carrental');
@@ -12,5 +12,6 @@ $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(P
 catch (PDOException $e)
 {
 exit("Error: " . $e->getMessage());
+
 }
 ?>
